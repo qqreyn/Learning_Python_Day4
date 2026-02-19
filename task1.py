9a9e90e58975c792244443df7b333dbcd2cb465a
qqreyn <duch.jan.8@gmail.com>
2026-02-19 16:11:35 +0100

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if (num > 0):
+    print("Number", num, "is a positive number")
+elif (num == 0):
+    print("Number", num, "is zero")
+else:
+    print("Number", num, "is a negative number")
