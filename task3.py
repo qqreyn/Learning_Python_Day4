2e304d7a182cf0b74f5412d368991913eb984a56
qqreyn <duch.jan.8@gmail.com>
2026-02-19 16:22:41 +0100

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,7 @@
+print("Is the number even or not?")
+num = int(input("Enter a number: "))
+
+if (num % 2 == 0):
+    print("Number (", num, ") is even")
+else:
+    print("Number (", num, ") is not even")
